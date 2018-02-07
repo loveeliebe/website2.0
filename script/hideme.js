d477322f239b8a8afbde30bb8a8f67e222a9b953
Pierre Le Fèvre <35996839+PierreLeFevre@users.noreply.github.com>
2018-02-07 21:47:49 +0100

    Update hideme.js

--- script/hideme.js
+++ script/hideme.js
@@ -6,13 +6,12 @@ $(document).ready(function() {
         //Kollar var varje element med klassen .hideme ligger
         $('.hideme').each( function(i){
             
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_object = $(this).offset().top + ( ( $(this).outerHeight() ) / 2 );
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             //Om hela objektet är i användarens viewport, lägg till klass viewed för senare hantering av css
             if( bottom_of_window > bottom_of_object ){
                $(this).addClass('viewed')
-
             }
             
         }); 
