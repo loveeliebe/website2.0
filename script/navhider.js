d429ebae4215b121e965c97c301c3be6655bd41e
Pierre Le Fèvre <35996839+PierreLeFevre@users.noreply.github.com>
2018-02-08 18:02:11 +0100

    Update navhider.js

--- script/navhider.js
+++ script/navhider.js
@@ -5,11 +5,11 @@ $(window).scroll(function(event){
     var st = $(this).scrollTop();
     if (st > lastScroll && st > minHeight){
         $('header').removeClass('nav-down').addClass('nav-up');
-        $('#backTop').fadeOut(250);
+        $('#backTop').fadeIn(750);
     } else {
         if(st + $(window).height() < $(document).height()) {
             $('header').removeClass('nav-up').addClass('nav-down');
-            $('#backTop').fadeIn(750);
+            $('#backTop').fadeOut(250);
         }
     }
     st = lastScroll
