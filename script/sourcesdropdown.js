e7c000b131d7472ba57534e4412894e0798ee2d5
Pierre Le Fèvre <35996839+PierreLeFevre@users.noreply.github.com>
2018-02-08 18:18:00 +0100

    Update sourcesdropdown.js

--- script/sourcesdropdown.js
+++ script/sourcesdropdown.js
@@ -1,13 +1,10 @@
 function main() {
-  $('.skillset').hide();
-  $('.skillset').fadeIn(1000);
+  $('.sources').hide();
   
-  $('.projects').hide();
-  
-  $('.projects-button').on('click', function() {
+  $('.sources-button').on('click', function() {
 		$(this).next().slideToggle(400);
     $(this).toggleClass('active');
-    $(this).text('Projects Viewed');
+    $(this).text('Sources Viewed');
 	});
 }
 
