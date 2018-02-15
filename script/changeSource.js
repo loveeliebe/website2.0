19ad148d61e92d5f3038752137676a9888567ca4
klingstrom <33989459+klingstrom@users.noreply.github.com>
2018-02-15 14:41:19 +0100

    Update changeSource.js

--- script/changeSource.js
+++ script/changeSource.js
@@ -7,7 +7,7 @@ $(window).scroll(function(){
   var viewport = $(window).scrollTop() + $(window).height();
   
   
-    if (viewport>article2_object && lastChecked != 2 && article3_object>viewport) {
+    if (viewport>article2_object && lastChecked != 2 && viewport>article3_object) {
       console.log(2);
       $("#header_background").attr('src', "Joel.mp4");
       lastChecked = 2;
@@ -21,7 +21,7 @@ $(window).scroll(function(){
  
   
 
-    if (viewport>article3_object && lastChecked != 3 && article2_object>viewport) {
+    if (viewport>article3_object && lastChecked != 3 && viewport>article2_object) {
       console.log(3);
       $("#header_background").attr('src', "Pierre.mp4");
       lastChecked = 3;
