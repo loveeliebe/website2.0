af8a153258ebfca725c053d45db9852816dc238c
Pierre Le Fèvre <35996839+PierreLeFevre@users.noreply.github.com>
2018-02-15 14:18:05 +0100

    Update changeSource.js

--- script/changeSource.js
+++ script/changeSource.js
@@ -1,13 +1,14 @@
 $(window).scroll(function(){
   var height = $(window).scrollTop();
-  
+  var lastChecked = 0;
   
   var article2_object = $("#article2").offset().top + ( ( $("#article2").outerHeight() ) / 2 );
   var article2_window = $(window).scrollTop() + $(window).height();
   
   
-    if (article2_window>article2_object) {
+    if (article2_window>article2_object && lastChecked != 2) {
       $("#header_background").attr('src', "Joel.mp4");
+      lastChecked = 2;
     };
   
   
@@ -18,8 +19,9 @@ $(window).scroll(function(){
  
   
 
-    if (article3_window>article3_object) {
+    if (article3_window>article3_object && lastChecked != 3) {
       $("#header_background").attr('src', "Pierre.mp4");
+      lastChecked = 3;
     };
   
  
