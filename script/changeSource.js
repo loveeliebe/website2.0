3f00c9531c2e7449e5ac86dea708b6bbed9fa10e
klingstrom <33989459+klingstrom@users.noreply.github.com>
2018-02-15 13:49:49 +0100

    Update changeSource.js

--- script/changeSource.js
+++ script/changeSource.js
@@ -10,4 +10,20 @@ $(window).scroll(function(){
       $("#header_background").attr('src', "Joel.mp4");
     };
   
+  
+  
+  
+    var article3_object = $("#article3").offset().top + ( ( $("#article3").outerHeight() ) / 2 );
+    var article3_window = $(window).scrollTop() + $(window).height();
+  
+  
+    if (article3_window>article3_object) {
+      $("#header_background").attr('src', "Pierre.mp4");
+    };
+  
+  
+  
+  
+  
+  
 })
